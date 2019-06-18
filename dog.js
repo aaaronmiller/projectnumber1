0826d706cea94e9ec12d160a5aff155ecd01b7f0
Christian O'Keefe <okeefe.christian94@gmail.com>
2019-06-18 12:37:46 -0700

    updated CSS for page

--- dog.js
+++ dog.js
@@ -42,7 +42,7 @@ $.ajax({
                 dogDescriptionToUse = results.animals[i].description
             }
 
-            var dogDiv = $(`<div class="card" style="width: 18rem;">`); //creats and store div tag also so that pictures show up next to each other
+            var dogDiv = $(`<div class="card dog-card" style="width: 18rem;" >`); //creats and store div tag also so that pictures show up next to each other
             var p = $("<p class = name>").text(results.animals[i].name);//creats p tag with rating
             var dogImage = $(`<img class="card-img-top dog-card-photo">`);//creates img tag
             var pawMeButton = $(`<button type="button" class="btn btn-primary dog-button" data-toggle="modal" data-target="#more-info-modal"> Paw Me! Paw Me!</button>`)//creates button tag called paw me that opens modal
@@ -121,105 +121,52 @@ $(function () {
                 $("#dogParks-appear-here").prepend(dogParkDiv);
             }
         });
-
-        // Northacres Park
-        // $.ajax({
-        //   url: "https://data.seattle.gov/resource/j9km-ydkc.json?name=Northacres Park",
-        //   type: "GET",
-        //   // data: {
-        //   //   "$limit": 100,
-        //   //   "$$app_token": "3R7XyKNOQtmYrrVv2BUjkrWzg"
-        //   // }
-        // }).done(function (data1) {
-        //   console.log("data from Northacres ", data1);
-
-        //   // Creating and storing a div tag
-        //   var dogParkDiv = $("<div>");
-
-        //   // Creating a paragraph tag with the result
-        //   var dogParkInfo = $("<p>").text("Dog Park Info: " + data1[0].name + ", Location: latitude " +data1[0].location.latitude+ " longitude "+ data1[0].location.latitude+ ", hours: "+ data1[0].hours);
-
-        //   // Appending the paragraph ta1 to the dogParkDiv
-        //   dogParkDiv.append(dogParkInfo);
-
-        //   // Prepending the dogParkDiv to the HTML page 
-        //   $("#dogParks-appear-here").prepend(dogParkDiv);
-        // });
-
-        // Woodland Park
-        // $.ajax({
-        //   url: "https://data.seattle.gov/resource/j9km-ydkc.json?name=Woodland Park",
-        //   type: "GET",
-        //   // data: {
-        //   //   "$limit": 5000,
-        //   //   "$$app_token": "3R7XyKNOQtmYrrVv2BUjkrWzg"
-        //   // }
-        // }).done(function (data2) {
-        //   console.log("data from Woodland ", data2);
-
-        //   // Creating and storing a div tag
-        //   var dogParkDiv = $("<div>");
-
-        //   // Creating a paragraph tag with the result
-        //   var dogParkInfo = $("<p>").text("Dog Park Info: " + data2[0].name + ", Location: latitude " +data2[0].location.latitude+ " longitude "+ data2[0].location.latitude+ ", hours: "+ data2[0].hours);
-
-        //   // Appending the paragraph ta1 to the dogParkDiv
-        //   dogParkDiv.append(dogParkInfo);
-
-        //   // Prepending the dogParkDiv to the HTML page 
-        //   $("#dogParks-appear-here").prepend(dogParkDiv);
-        // });
-
-        // Regrade Park
-        // $.ajax({
-        //   url: "https://data.seattle.gov/resource/j9km-ydkc.json?name=Regrade Park",
-        //   type: "GET",
-        //   // data: {
-        //   //   "$limit": 5000,
-        //   //   "$$app_token": "3R7XyKNOQtmYrrVv2BUjkrWzg"
-        //   // }
-        // }).done(function (data3) {
-        //   console.log("data from Regarade ", data3);
-
-        //   // Creating and storing a div tag
-        //   var dogParkDiv = $("<div>");
-
-        //   // Creating a paragraph tag with the result
-        //   var dogParkInfo = $("<p>").text("Dog Park Info: " + data3[0].name + ", Location: latitude " +data3[0].location.latitude+ " longitude "+ data3[0].location.latitude+ ", hours: "+ data3[0].hours);
-
-        //   // Appending the paragraph ta1 to the dogParkDiv
-        //   dogParkDiv.append(dogParkInfo);
-
-        //   // Prepending the dogParkDiv to the HTML page 
-        //   $("#dogParks-appear-here").prepend(dogParkDiv);
-        // });
-
-        // Magnolia Park
-        // $.ajax({
-        //   url: "https://data.seattle.gov/resource/j9km-ydkc.json?name=Magnolia Park",
-        //   type: "GET",
-        //   // data: {
-        //   //   "$limit": 5000,
-        //   //   "$$app_token": "3R7XyKNOQtmYrrVv2BUjkrWzg"
-        //   // }
-        // }).done(function (data4) {
-        //   console.log("data from Magnolia ", data4);
-
-        //   // Creating and storing a div tag
-        //   var dogParkDiv = $("<div>");
-
-        //   // Creating a paragraph tag with the result
-        //   var dogParkInfo = $("<p>").text("Dog Park Info: " + data4[0].name + ", Location: latitude " +data4[0].location.latitude+ " longitude "+ data4[0].location.latitude+ ", hours: "+ data4[0].hours);
-
-        //   // Appending the paragraph ta1 to the dogParkDiv
-        //   dogParkDiv.append(dogParkInfo);
-
-        //   // Prepending the dogParkDiv to the HTML page 
-        //   $("#dogParks-appear-here").prepend(dogParkDiv);
-        // });
-
-        // End of onclick function
     })
 
     // End of function
 })
+var settings = {
+    "async": true,
+    "crossDomain": true,
+    // "url": "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=dog+restaurant&location=bellevue,WA",
+    "url": "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=dog+restaurant&latitude=47.589982&longitude=-122.297814",
+    "method": "GET",
+    "headers": {
+        // "accept": "application/json",
+        // "Access-Control-Allow-Origin": "*",
+        "Authorization": "Bearer SDAEnMNqSOPl9_I9468qC_1PDuSvS67-h-HCkR6lPtwoYMA1bqU1yVT5pP1SUh_Cr3j4GucEh32EuhxxdUXZn7vBtrJ7V7zaD3ZgWmFIxsIDR0B3BY9ix3QxmeyLXHYx",
+        "cache-control": "no-cache",
+        
+    }
+}
+    
+$.ajax(settings).then(function (response) {
+    
+    var bizName = [];
+    var bizLong = [];
+    var bizLat = [];
+    var bizAddressStreet = []
+    var bizAddressState = [];
+    var bizRating = [];
+    var bizPrice = [];
+    var bizUrl = [];
+    var bizPhone = [];
+    
+    console.log(response);
+    
+    for (i=0; i<7;i++) { 
+
+    bizName[i] = response.businesses[i].name;
+    bizLong[i] = response.businesses[i].coordinates.longitude;
+    bizLat[i] = response.businesses[i].coordinates.latitude;
+    bizAddressStreet[i] = response.businesses[i].location.display_address[0];
+    bizAddressState[i] = response.businesses[i].location.display_address[1];
+    bizRating[i] = response.businesses[i].rating;
+    bizPrice[i] = response.businesses[i].price;
+    bizUrl[i] = response.businesses[i].url;
+    bizPhone[i] = response.businesses[i].display_phone;
+
+    $('.card' + i + '-title').html('<a href="' + bizUrl[i] +'">' + bizName[i] + '</a>');
+    $('.card' + i + '-text').html('<br>' + bizAddressStreet[i] + '<br>' + bizAddressState[i] +'<p>' + bizPhone[i] +'</p><p>' + 'Rating:' + bizRating[i] +'</p><p>' + 'Price:' + bizPrice[i] +'</p>'); 
+    }
+});
