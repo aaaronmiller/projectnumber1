321e84a960c4c7b4a88efd8e269f166eb3717e31
Kim C <kimkly.chau@gmail.com>
2019-06-18 14:23:31 -0700

    updated dog.js

--- dog.js
+++ dog.js
@@ -1,97 +1,139 @@
-// Dog Adoption
-$.ajax({
-    method: "POST",
-    url: "https://api.petfinder.com/v2/oauth2/token",
-    data: {
-        grant_type: "client_credentials",
-        client_id: "wCs60AHgyCrbWG1krL41opIHyPlrhUFLsQNWwLz8k5rmzRrEGg",
-        client_secret: "7zyl5VWLecLoxMkrHsqtcZJjfWE0OTdaggVNxLFC"
+// Google Maps
+var map, infoWindow;
+
+// Initialize and add the map
+function initMap() {
+    //creates map on the screen, using Seattle lat/lng asks user location and takes them there at zoom 12
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: 47.6062, lng: 122.3321 },
+        zoom: 12
+    });
+    infoWindow = new google.maps.InfoWindow;
+
+    //if location is accessed, takes the screen to the users location.
+    if (navigator.geolocation) {
+        console.log('geolocation exists')
+        navigator.geolocation.getCurrentPosition(function (position) {
+            console.log("inside success callback")
+            var pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Location found.');
+            infoWindow.open(map);
+            map.setCenter(pos);
+        }, function () {
+            handleLocationError(true, infoWindow, map.getCenter());
+        });
+    } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
     }
-}).then(function (response) {
-    var accessToken = response.access_token
+}
+// if location isnt accessed gives error
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+        'Error: The Geolocation service failed.' :
+        'Error: Your browser doesn\'t support geolocation.');
+    infoWindow.open(map);
+}
+
+$(function () {
+    // Dog Adoption
     $.ajax({
-        method: "GET",
-        headers: {
-            Authorization: "Bearer " + accessToken,
-        },
-        url: " https://api.petfinder.com/v2/animals?type=dog&limit=4"
-    }).then(function (data) {
-
-        var results = data;//store data from ajax request in result var
-        var pictureDefault = "https://www.logoground.com/uploads/20172422017-06-034038115happy-cartoon-dog-logo.jpg"
-        var photoToUse = ""
-        var dogDescriptionDefault = "N/A"
-        var dogDescriptionToUse = ""
-        var dogSound = new Audio();
-        dogSound.src = "Dog Woof.mp3"
-
-
-        console.log(data)
-        //if theres no image the var pictureDefault will display the default pic
-        for (var i = 0; i < results.animals.length; i++) {
-            if (results.animals[i].photos.length === 0) {
-                pictureToUse = pictureDefault;
-
-            }
-            else {
-                pictureToUse = results.animals[i].photos[0].medium;
-            }//if theres no description available, N/A will be display
-            if (results.animals[i].description.length === 0) {
-                dogDescriptionToUse = dogDescriptionDefault;
-            }
-            else {
-                dogDescriptionToUse = results.animals[i].description
-            }
-
-            var dogDiv = $(`<div class="card" style="width: 18rem;">`); //creats and store div tag also so that pictures show up next to each other
-            var p = $("<p class = name>").text(results.animals[i].name);//creats p tag with rating
-            var dogImage = $(`<img class="card-img-top dog-card-photo">`);//creates img tag
-            var pawMeButton = $(`<button type="button" class="btn btn-primary dog-button" data-toggle="modal" data-target="#more-info-modal"> Paw Me! Paw Me!</button>`)//creates button tag called paw me that opens modal
-
-            //creates attribute for Paw M button
-            pawMeButton.attr("pic", pictureToUse);
-            pawMeButton.attr("name", results.animals[i].name);
-            pawMeButton.attr("breeds", results.animals[i].breeds.primary);
-            pawMeButton.attr("gender", results.animals[i].gender);
-            pawMeButton.attr("description", results.animals[i].description);
-            pawMeButton.attr("src", results.animals[i].url);
-
-
-            dogImage.attr("src", pictureToUse);
-            //adds paragraph and image tag to dogDiv
-            dogDiv.append(dogImage);
-            dogDiv.append(p);
-            dogDiv.append(pawMeButton);//adds button to dogDiv
-            $("#dog-gallery-container").prepend(dogDiv);//adds the dogDiv (div class) before the p tag
-
-            $('.dog-button').on('click', (e) => {
-                console.log(e)
-
-
-                //when you click on the "paw me" button the modal with pop up with the info below.
-                $("#dog-image").attr("src", e.target.attributes.pic.nodeValue)
-                $("#dog-name").text(e.target.attributes.name.nodeValue)
-                $("#dog-breed").html("<b> Breed:  </b>" + e.target.attributes.breeds.nodeValue)
-                $("#dog-gender").html("<b> Gender: </b>" + e.target.attributes.gender.nodeValue)
-                $("#dog-description").html("<b> About Me: </b>" + e.target.attributes.description.nodeValue)
-                $("#dog-link").text(e.target.attributes.src.nodeValue)
-                $("#dog-link").attr("href", e.target.attributes.src.nodeValue)
-
-
-            })
+        method: "POST",
+        url: "https://api.petfinder.com/v2/oauth2/token",
+        data: {
+            grant_type: "client_credentials",
+            client_id: "wCs60AHgyCrbWG1krL41opIHyPlrhUFLsQNWwLz8k5rmzRrEGg",
+            client_secret: "7zyl5VWLecLoxMkrHsqtcZJjfWE0OTdaggVNxLFC"
         }
+    }).then(function (response) {
+        var accessToken = response.access_token
+        $.ajax({
+            method: "GET",
+            headers: {
+                Authorization: "Bearer " + accessToken,
+            },
+            url: " https://api.petfinder.com/v2/animals?type=dog&limit=8"
+        }).then(function (data) {
 
+            var results = data;//store data from ajax request in result var
+            var pictureDefault = "https://www.logoground.com/uploads/20172422017-06-034038115happy-cartoon-dog-logo.jpg";
+            var pictureToUse = "";
+            var dogDescriptionDefault = "N/A";
+            var dogDescriptionToUse = "";
+
+            var dogSound = new Audio();
+            dogSound.src = "Dog Woof.mp3"
+
+
+            console.log(data)
+            //if theres no image the var pictureDefault will display the default pic
+            for (var i = 0; i < results.animals.length; i++) {
+                if (results.animals[i].photos.length === 0) {
+                    pictureToUse = pictureDefault;
+
+                }
+                else {
+                    pictureToUse = results.animals[i].photos[0].medium;
+                }//if theres no description available, N/A will be display
+                if (results.animals[i].description.length === 0) {
+                    dogDescriptionToUse = dogDescriptionDefault;
+                }
+                else {
+                    dogDescriptionToUse = results.animals[i].description
+                }
+
+                var dogDiv = $(`<div class="card" style="width: 18rem;">`); //creats and store div tag also so that pictures show up next to each other
+                var p = $("<p class = name>").text(results.animals[i].name);//creats p tag with rating
+                var dogImage = $(`<img class="card-img-top dog-card-photo">`);//creates img tag
+                var pawMeButton = $(`<button type="button" class="btn btn-primary dog-button" data-toggle="modal" data-target="#more-info-modal"> Paw Me! Paw Me!</button>`)//creates button tag called paw me that opens modal
+
+                //creates attribute for pawMe button
+                pawMeButton.attr("pic", pictureToUse);
+                pawMeButton.attr("name", results.animals[i].name);
+                pawMeButton.attr("breeds", results.animals[i].breeds.primary);
+                pawMeButton.attr("gender", results.animals[i].gender);
+                pawMeButton.attr("description", results.animals[i].description);
+                pawMeButton.attr("src", results.animals[i].url);
+
+
+                dogImage.attr("src", pictureToUse);
+                //adds paragraph and image tag to dogDiv
+                dogDiv.append(dogImage);
+                dogDiv.append(p);
+                dogDiv.append(pawMeButton);//adds button to dogDiv
+                $("#dog-gallery-container").prepend(dogDiv);//adds the dogDiv (div class) before the p tag
+
+                $('.dog-button').on('click', (e) => {
+                    console.log(e)
+
+
+                    //when you click on the "paw me" button the modal with pop up with the info below.
+                    $("#dog-image").attr("src", e.target.attributes.pic.nodeValue)
+                    $("#dog-name").text(e.target.attributes.name.nodeValue)
+                    $("#dog-breed").html("<b> Breed:  </b>" + e.target.attributes.breeds.nodeValue)
+                    $("#dog-gender").html("<b> Gender: </b>" + e.target.attributes.gender.nodeValue)
+                    $("#dog-description").html("<b> About Me: </b>" + e.target.attributes.description.nodeValue)
+                    $("#dog-link").text(e.target.attributes.src.nodeValue)
+                    $("#dog-link").attr("href", e.target.attributes.src.nodeValue)
+
+
+                })
+            }
+
+
+        })
 
     })
 
-})
-
-// Dog Parks
-$(function () {
-
-    // Adding click event listener to all buttons
-    $(document).on("click", ".btn-primary", function () {
-
+    // Dog Parks
+    // Adding click event listener to all buttons <<TODO Change this to only the Park Button>>
+    $(document).on("click", "#park-button", function () {
         // Empty html <div>
         $("#dogParks-appear-here").empty();
 
@@ -99,29 +141,54 @@ $(function () {
         $.ajax({
             url: "https://data.seattle.gov/resource/j9km-ydkc.json?feature_desc=Dog Off Leash Area",
             type: "GET",
-            // data: {
-            // "$limit": 100,
-            // "$$app_token": "3R7XyKNOQtmYrrVv2BUjkrWzg"
-            // }
         }).then(function (data) {
-            console.log("data from seattle parks ", data);
+            console.log("data from Seattle Parks", data);
 
             // Looping through each result item
             for (var i = 0; i < data.length; i++) {
 
                 // Creating and storing a div tag
-                var dogParkDiv = $("<div>");
+                var dogParkDiv = $(`<div class="card" style="width: 18rem;">`);
 
                 // Creating a paragraph tag with the result info
-                var dogParkInfo = $("<p>").text("Dog Park Info: " + data[i].name + ", Location: latitude " + data[i].location.latitude + " longitude " + data[i].location.latitude + ", hours: " + data[i].hours);
+                var dogParkInfo = $("<p>").text("Dog Park Info: " + data[i].name + " hours: " + data[i].hours);
+
+                console.log("lat " + data[i].location.latitude);
+                console.log("lng " + data[i].location.longitude);
+
+                // Auto generate dog park pins
+                var infowindow = new google.maps.InfoWindow({
+                    content: data[i].name
+                });
+
+                console.log(typeof data[i].location.latitude)
+                console.log(typeof data[i].location.longitude)
+
+                var marker = new google.maps.Marker({
+                    position: { lat: parseFloat(data[i].location.latitude), lng: parseFloat(data[i].location.longitude) },
+                    map: map,
+                    title: data[i].name,
+
+                    icon: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png'
+                });
+
+                marker.addListener('click', function () {
+                    infowindow.open(map, marker);
+                });
 
                 // Appending the paragraph tag to the dogParkDiv
                 dogParkDiv.append(dogParkInfo);
 
                 // Prepending the dogParkDiv to the HTML page 
-                $("#dogParks-appear-here").prepend(dogParkDiv);
+                $(".add-card").prepend(dogParkDiv);
+
+                // End of for loop
             }
+            // End of Seattle Parks AJAX data
         });
+
+
+
 
         // Northacres Park
         // $.ajax({
@@ -219,8 +286,10 @@ $(function () {
         //   $("#dogParks-appear-here").prepend(dogParkDiv);
         // });
 
+
+
+
         // End of onclick function
     })
-
-    // End of function
+    // End of document ready function
 })
