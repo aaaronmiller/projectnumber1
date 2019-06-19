e2f348737f4e8168c742e36d93d076215f3e57ec
Monica Parente <mon8806@gmail.com>
2019-06-18 19:11:55 -0700

    add

--- dog.js
+++ dog.js
@@ -126,7 +126,24 @@ $(function () {
                 $("#dogParks-appear-here").prepend(dogParkDiv);
             }
         });
-
+        
+        $('.owl-carousel').owlCarousel({
+            items:1,
+            merge:true,
+            loop:true,
+            margin:10,
+            video:true,
+            lazyLoad:true,
+            center:true,
+            responsive:{
+                480:{
+                    items:2
+                },
+                600:{
+                    items:4
+                }
+            }
+        })
         // Northacres Park
         // $.ajax({
         //   url: "https://data.seattle.gov/resource/j9km-ydkc.json?name=Northacres Park",
